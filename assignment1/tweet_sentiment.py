e2ec311299f9d896a57538b8e2a9f264056e94e4
Isha Kaur Somani <ishakaur@gmail.com>
2015-09-10 14:16:36 -0400

    Calculating tweet sentiment

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,32 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def parse_scores(afinnfile):
+    afinnfile.seek(0)
+    scores = {} # initialize an empty dictionary
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term.lower()] = int(score)  # Convert the score to an integer.
+    #print scores.items() # Print every (term, score) pair in the dictionary
+    return scores
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def print_tweet_scores(tweet_file, scores):
+    tweet_file.seek(0)
+    for tweet in tweet_file:
+        tweetjson = json.loads(tweet)
+        if 'text' in tweetjson.keys():
+            tweet_text = tweetjson['text']
+            words = tweet_text.lower().split()
+            score = sum([scores[word] if word in scores else 0 for word in words])
+            print score
+        else:
+            print 0
 
 def main():
-    sent_file = open(sys.argv[1])
+    sentiment_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    scores = parse_scores(sentiment_file)
+    print_tweet_scores(tweet_file, scores)
 
 if __name__ == '__main__':
     main()
